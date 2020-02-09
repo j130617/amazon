675742603e9a3ecda46e8d06cc4f0848113ef0ed
j130617 <39986473+j130617@users.noreply.github.com>
2020-02-09 13:27:51 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,3 +1,3 @@
 /*var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HlindMPfrk1y17ZkPbQRUmLU07tDiWry92AkFWlsCNI/edit#gid=0';*/
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MENuQDTzn6unDhXt4SoPQi8mncsZ6u75f4MWk1rPV7o/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1c-f9RnETuK3m0Zlip8jTrHyq8930OCG6qildmgokTUE/edit#gid=0';
 var googleURL = 'https://www.google.com';
